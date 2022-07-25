32f0c7284f299382ba74304ec1b5767f8d9c0be2
Kisal De Silva <kisalsilva1020@outlook.com>
2022-07-25 22:39:13 +0530

    add Reset button

--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -4,28 +4,36 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     Counters: [
-      { id: 1, value: 4 },
+      { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
     ],
   };
 
-  handleDelete = counterId => {
-    const Counters = this.state.Counters.filter(c => c.id !== counterId);
-    this.setState({ Counters});
+  handleReset = () => {
+    const Counters = this.state.Counters.map(c => {
+      c.value = 0;
+      return c;
+    }
+      );
+      this.setState( {Counters });
+  };
+
+  handleDelete = (counterId) => {
+    const Counters = this.state.Counters.filter((c) => c.id !== counterId);
+    this.setState({ Counters });
   };
 
   render() {
     return (
       <div>
+        <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
         {this.state.Counters.map((counter) => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
-            value={counter.value}
-            id={counter.id}
-          ></Counter>
+            counter={counter}/>
         ))}
       </div>
     );
